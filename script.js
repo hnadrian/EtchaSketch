84df6d8e12e106c2dd000c14d722a12fc7e03b98
hnadrian <hngt.adrian@gmail.com>
2021-05-14 02:24:33 -0700

    Add size Slider and text box, clear button, hook up js event connection between slider and text box

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector('#container');
+const sizeText = document.querySelector('#size-text');
+const clearButton = document.querySelector('#clear-button');
+const sizeSlider =  document.querySelector('#size-slider');
+sizeSlider.addEventListener('input',updateGridSize);
 
 function makeSquareDivs(size) {
     for (let i = 0; i < (size * size); i++) {
@@ -9,6 +13,11 @@ function makeSquareDivs(size) {
         container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
         container.appendChild(gridBlock).classList.add('block');
     }
+}
+
+function updateGridSize(e) {
+    let newSize = e.target.value;
+    sizeText.value = newSize;
 }
 
 makeSquareDivs(16);
