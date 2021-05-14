8ea110422ec64dfcdabc40851322d86d1c6c08b9
hnadrian <hngt.adrian@gmail.com>
2021-05-14 15:22:59 -0700

    Added event for mouse click to toggle drawing mode

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const container = document.querySelector('#container');
+container.addEventListener('click', toggleDrawMode);
 const sizeText = document.querySelector('#size-text');
 const clearButton = document.querySelector('#clear-button');
 const sizeSlider =  document.querySelector('#size-slider');
 sizeSlider.addEventListener('input',updateGridSize); //Activiate as range slides
 sizeSlider.addEventListener('change',updateGridSizeDisplay); //Activate as range changed (mouse released)
 
+const DEFAULT_COLOR = "white";
+let drawStarted = false;
+
 function makeSquareDivs(size) {
     for (let i = 0; i < (size * size); i++) {
         const gridBlock = document.createElement('div');
-
         gridBlock.style.border = '0.5px solid red';
         container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -26,8 +29,23 @@ function updateGridSizeDisplay(e) {
     makeSquareDivs(e.target.value);
 }
 
+function toggleDrawMode(e) {
+    let blockList = document.querySelectorAll('.block');
+    if (!drawStarted){
+        blockList.forEach(block => {
+            block.addEventListener('mouseenter', draw);
+        });
+        drawStarted = true;
+    } else {
+        blockList.forEach(block => {
+            block.removeEventListener('mouseenter', draw);
+        });
+        drawStarted = false;
+    }
+}
+
 function draw(e) {
-    
+    e.target.style.backgroundColor = 'blue';
 }
 
 function clearDrawing(e) {
@@ -40,4 +58,4 @@ function clearGrid() {
     }
 }
 
-makeSquareDivs(16);
+makeSquareDivs(15);
