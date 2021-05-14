a96920bcfa6f62fb23aaa3b7d52a25f3b833cc9e
hnadrian <hngt.adrian@gmail.com>
2021-05-14 10:08:40 -0700

    Add event to change grid size with range slider HTML,JS

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const container = document.querySelector('#container');
 const sizeText = document.querySelector('#size-text');
 const clearButton = document.querySelector('#clear-button');
 const sizeSlider =  document.querySelector('#size-slider');
-sizeSlider.addEventListener('input',updateGridSize);
+sizeSlider.addEventListener('input',updateGridSize); //Activiate as range slides
+sizeSlider.addEventListener('change',updateGridSizeDisplay); //Activate as range changed (mouse released)
 
 function makeSquareDivs(size) {
     for (let i = 0; i < (size * size); i++) {
@@ -18,6 +19,25 @@ function makeSquareDivs(size) {
 function updateGridSize(e) {
     let newSize = e.target.value;
     sizeText.value = newSize;
+}
+
+function updateGridSizeDisplay(e) {
+    clearGrid();
+    makeSquareDivs(e.target.value);
+}
+
+function draw(e) {
+    
+}
+
+function clearDrawing(e) {
+
+}
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
 
 makeSquareDivs(16);
